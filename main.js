750244faf6f1908f38a4bbcb770a163abc01dced
ChenHuang <alan.tolearn@gmail.com>
2020-10-10 12:57:55 -0400

    Add custom component without implementation

--- main.js
+++ main.js
@@ -9,13 +9,23 @@
 //     "class": "child"
 //   }, "Hello"), createElement("span", null, "World"));
 
-function createElement(tagName, attributes, ...children) {
-    let el = document.createElement(tagName)
+function createElement(type, attributes, ...children) {
+    // TODO: el created by createElement could be DOM elemenet, text node or custom element in this case, need to create generic interface for all
+    // The interface will have setAttribute, appendChild methods
+    let el
+    if (typeof type === "string") {
+        // TODO: use adapter pattern with a generic interface
+        el = document.createElement(type)
+    } else {
+        // TODO: use adapter pattern with a generic interface
+        el = new type
+    }
     for (let attrName in attributes) {
         el.setAttribute(attrName, attributes[attrName])
     }
     for (let child of children) {
         if (typeof child === 'string') {
+            // TODO: use adapter pattern with a generic interface
             child = document.createTextNode(child)
         }
         el.appendChild(child)
@@ -23,8 +33,11 @@ function createElement(tagName, attributes, ...children) {
     return el
 }
 
-let a = <div id="1" class="parent">
+class MyComponent {
+}
+
+let a = <MyComponent id="1" class="parent">
     <div id="2" class="child">Hello</div>
     <span>World</span>
-</div>
+</MyComponent>
 console.log(a)
